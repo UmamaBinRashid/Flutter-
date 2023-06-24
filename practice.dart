5c080aac5925d8d110b348df8e953d4a5686cbd9
UmamaBinRashid <princeumama4@gmail.com>
2023-06-24 21:03:44 +0500

    add .dart file

---
+++ practice.dart
@@ -0,0 +1,28 @@
+void main(){
+  //POST AND PREINCREMENT
+   //int abc=1;
+   //var c=abc++;
+   //print("abc value : $abc");
+   //print("c value : $c");//pehle  c me store abc hoga baad me abc +1 hoga 
+   //var preinc=++abc;
+   //print("abc value : $abc");
+   //print("Preincrement value : $preinc");
+    //pehle abc me +1 hoga then  wo a variable me store hoga 
+    //matlab pehle abc 2 hojaye ga phir a me 2 store hoga
+   //for loop
+  // for(int i=1;i<=10;i++){
+    //print(i);
+   //}
+   //int no=2;
+   //for(int i=1;i<=10;i++){
+    //print(no);
+   //}
+  // int no=2;
+   //for(int i=1;i<=10;i++){
+    //print(no*i);
+   //}
+  int no=2;
+   for(int i=1;i<=10;i++){
+   print("$no x $i = ${no * i}");
+   }
+}
